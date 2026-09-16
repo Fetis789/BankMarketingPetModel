aaa7e144c5f88f2ebd727b9fad174336dd0b5241
Fetis789 <fetisov.grisha789@gmail.com>
2026-09-16 23:29:22 +0300

    Started pydantic Features

--- src/bank/service/app.py
+++ src/bank/service/app.py
@@ -13,4 +13,11 @@ from pydantic import BaseModel, Field
 from churn import db
 from churn.config import settings
 
-class Features
+class Features(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    euribor3m: float = Field(ge=0, description="EURIBOR 3M rate")
+    nr.employeed: float = Field(ge=0, description="Number of employed")
+    month: Literal["jan","feb","mar","apr","may","jun","jul","aug",
+        "sep", "oct","nov","dec"] = Field(description="Month of the last contact")
+    
