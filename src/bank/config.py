56e63400b8b491491d60ebf6690b3ad3670f217c
Fetis789 <fetisov.grisha789@gmail.com>
2026-09-19 19:40:23 +0300

    Changed dockerignore aand settings

--- src/bank/config.py
+++ src/bank/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import  BaseSettings
 
 class Settings(BaseSettings):
-    model_path: str = "artefacts/bank_marketing_catboost_bundle.joblib"
-    database_url: str = "postgresql://postgres:postgres@localhost:5432/bank_marketing"
+    model_path: str = "artefacts/bank_marketing_nodel_catboost_bundle.joblib"
+    database_url: str | None = None
     log_level: str = "INFO"
 
     model_config = {"env_file": ".env"}
